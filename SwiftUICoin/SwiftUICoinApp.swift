82d9427ec8451a37487e7a5d05ff5656f814d291
WooMin <hanulbom@gmail.com>
2022-09-11 23:29:52 +0900

    Initial Commit

---
+++ SwiftUICoin/SwiftUICoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinApp.swift
+//  SwiftUICoin
+//
+//  Created by Woo Min on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
