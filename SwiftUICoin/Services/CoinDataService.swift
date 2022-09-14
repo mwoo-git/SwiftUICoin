0534bb8b2090cbac474fd2d7750d19f93558e580
WooMin <hanulbom@gmail.com>
2022-09-14 16:39:04 +0900

    Networking Manager 재사용 가능한 네트워킹 매니저

--- SwiftUICoin/Services/CoinDataService.swift
+++ SwiftUICoin/Services/CoinDataService.swift
@@ -22,27 +22,11 @@ class CoinDataService {
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default)) // 백그라운드 스레드에서 실행하라. 기본적으로 백그라운드 스레드에서 작업이 되지만 확실히 명시해준다.
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode == 200 else {
-                          throw URLError(.badServerResponse)
-                      }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main) //메인 스레드에서 실행하라
+        coinSubscription = NetWorkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnCoins) in
+            .sink(receiveCompletion: NetWorkingManager.handleCompletion, receiveValue: { [weak self] (returnCoins) in
                 self?.allCoins = returnCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
---
+++ SwiftUICoin/Utilities/NetworkingManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingManager.swift
+//  SwiftUICoin
+//
+//  Created by Woo Min on 2022/09/14.
+//  재사용 가능한 네트워킹 매니저
+
+import Foundation
+import Combine
+
+class NetWorkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response form URL: \(url)"
+                // \(url )로 어떤 url에서 에러가 발생했는지 알 수 있습니다.
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode == 200 else {
+                  throw NetworkingError.badURLResponse(url: url)
+              }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
