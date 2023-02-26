16e7e2b28da8065be9f8ee53cdae9708765fe797
WooMin <hanulbom@gmail.com>
2023-02-26 17:05:38 +0900

    refactor: SortOptionView 의 중복되는 코드를 함수로 정리하였습니다.

--- SwiftUICoin/Core/Home/Views/SortOptionView.swift
+++ SwiftUICoin/Core/Home/Views/SortOptionView.swift
@@ -11,118 +11,40 @@ struct SortOptionView: View {
     
     @EnvironmentObject var viewModel: HomeViewModel
     
+    @State private var scrollViewProxy: ScrollViewProxy? = nil
+    
     var body: some View {
         ScrollViewReader { proxy in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 10) {
                     
-                    let vertical: CGFloat = 8
-                    let horizontal: CGFloat = 13
-                    
-                    HStack(spacing: 5) {
-                        Image(systemName: viewModel.sortOption != .favorite ? "star" : "star.fill")
-                            .foregroundColor(viewModel.sortOption != .favorite ? Color.theme.textColor : Color.theme.binanceColor)
-                        Text("관심목록")
-                            .foregroundColor((viewModel.sortOption == .favorite) ? Color.white : Color.white)
-                    }
-                    .id("FAVORITE")
-                    .padding(.vertical, vertical)
-                    .padding(.trailing, horizontal)
-                    .padding(.leading, 6)
-                    .background(
-                        Color.theme.sortOptionColor
-                    )
-                    .cornerRadius(3)
-                    .onTapGesture {
-                        if viewModel.sortOption != .favorite {
-                            viewModel.sortOption = .favorite
-                        }
-                        withAnimation(.easeInOut) {
-                            proxy.scrollTo("FAVORITE", anchor: .topLeading)
-                        }
-                    }
+                    button(for: .favorite, label: "관심목록")
+                        .padding(.leading, 6)
                     
                     Divider()
                     
-                    HStack(spacing: 0) {
-                        Text("시가총액")
-                            .foregroundColor((viewModel.sortOption == .rank) ? Color.theme.background : Color.theme.textColor)
-                    }
-                    .id("MARKET_CAP")
-                    .padding(.vertical, vertical)
-                    .padding(.horizontal, horizontal)
-                    .background(
-                        viewModel.sortOption == .rank ? Color.theme.textColor : Color.theme.sortOptionColor
-                    )
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color(.systemGray4), lineWidth: 3)
-                    )
-                    .cornerRadius(50)
-                    .onTapGesture {
-                        if viewModel.sortOption != .rank {
-                            viewModel.sortOption = .rank
-                        }
-                        withAnimation(.easeInOut) {
-                            proxy.scrollTo("MARKET_CAP", anchor: .topLeading)
-                        }
-                    }
+                    button(for: .rank, label: "시가총액")
+                        .overlay(selectedBorder(for: .rank), alignment: .bottom)
+                        .cornerRadius(50)
                     
-                    HStack(spacing: 0) {
-                        Text("상승")
-                            .foregroundColor((viewModel.sortOption == .priceChangePercentage24H) ? Color.theme.background : Color.theme.textColor)
-                    }
-                    .id("PRICE_UP")
-                    .padding(.vertical, vertical)
-                    .padding(.horizontal, horizontal)
-                    .background(
-                        viewModel.sortOption == .priceChangePercentage24H ? Color.theme.textColor : Color.theme.sortOptionColor
-                    )
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color(.systemGray4), lineWidth: 3)
-                    )
-                    .cornerRadius(50)
-                    .onTapGesture {
-                        if viewModel.sortOption != .priceChangePercentage24H {
-                            viewModel.sortOption = .priceChangePercentage24H
-                        }
-                        withAnimation(.easeInOut) {
-                            proxy.scrollTo("PRICE_UP", anchor: .topLeading)
-                        }
-                    }
+                    button(for: .priceChangePercentage24H, label: "상승")
+                        .overlay(selectedBorder(for: .priceChangePercentage24H), alignment: .bottom)
+                        .cornerRadius(50)
                     
-                    HStack(spacing: 0) {
-                        Text("하락")
-                            .foregroundColor((viewModel.sortOption == .priceChangePercentage24HReversed) ? Color.theme.background : Color.theme.textColor)
-                    }
-                    .id("PRICE_DOWN")
-                    .padding(.vertical, vertical)
-                    .padding(.horizontal, horizontal)
-                    .background(
-                        viewModel.sortOption == .priceChangePercentage24HReversed ? Color.theme.textColor : Color.theme.sortOptionColor
-                    )
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color(.systemGray3), lineWidth: 3)
-                    )
-                    .cornerRadius(50)
-                    .onTapGesture {
-                        if viewModel.sortOption != .priceChangePercentage24HReversed {
-                            viewModel.sortOption = .priceChangePercentage24HReversed
-                        }
-                        withAnimation(.easeInOut) {
-                            proxy.scrollTo("PRICE_DOWN", anchor: .topLeading)
-                        }
-                    }
+                    button(for: .priceChangePercentage24HReversed, label: "하락")
+                        .overlay(selectedBorder(for: .priceChangePercentage24HReversed), alignment: .bottom)
+                        .cornerRadius(50)
                 }
                 .font(.system(size: 15, weight: .regular))
-                .foregroundColor(Color.theme.accent)
+                .foregroundColor(.theme.accent)
+                .background(Color.theme.background)
+                .onAppear {
+                    self.scrollViewProxy = proxy
+                }
+                .padding(.horizontal, 10) // Add horizontal padding to HStack
             }
-            .padding(.horizontal)
             .padding(.bottom, 8)
         }
-        .background(Color.theme.background)
     }
 }
 
@@ -135,5 +57,53 @@ struct SortOptionBarView_Previews: PreviewProvider {
                 .preferredColorScheme(.dark)
                 .previewLayout(.sizeThatFits)
         }
+    }
+}
+
+private extension SortOptionView {
+    func button(for sortOption: SortOption, label: String) -> some View {
+        let isSelected = viewModel.sortOption == sortOption
+        
+        return Button(action: {
+            if viewModel.sortOption != sortOption {
+                viewModel.sortOption = sortOption
+            }
+            
+            withAnimation(.easeInOut) {
+                scrollViewProxy?.scrollTo(sortOption, anchor: .topLeading)
+            }
+        }, label: {
+            if sortOption == .favorite {
+                HStack(spacing: 5) {
+                    Image(systemName: viewModel.sortOption != .favorite ? "star" : "star.fill")
+                        .foregroundColor(viewModel.sortOption != .favorite ? .theme.textColor : .theme.binanceColor)
+                    Text(label)
+                        .foregroundColor(Color.theme.textColor)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal, 13)
+                .padding(.leading, -6)
+                .background(Color.theme.sortOptionColor)
+                .cornerRadius(3)
+            } else {
+                HStack {
+                    Text(label)
+                        .foregroundColor(isSelected ? Color.theme.background : Color.theme.textColor)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal, 13)
+                .background(isSelected ? Color.theme.textColor : Color.theme.sortOptionColor)
+                .cornerRadius(3)
+            }
+            
+        })
+            .id(sortOption)
+    }
+    
+    func selectedBorder(for sortOption: SortOption) -> some View {
+        let isSelected = viewModel.sortOption == sortOption
+        
+        return RoundedRectangle(cornerRadius: 50)
+            .stroke(isSelected ? Color.theme.background : Color(.systemGray4), lineWidth: 3)
     }
 }
